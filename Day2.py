70fb508f338843bc6e9f43e90e5c8ca76930dd23
Eduardo Argente <eduardoargente.dev@gmail.com>
2024-11-06 12:08:44 -0600

    Second Project

--- Day2.py
+++ Day2.py
@@ -4,6 +4,8 @@ bill = float(input("Whats the total bill?: "))
 tip = int(input("How much tip would you like to give?: "))
 people = int(input("How many people to split the bill?: "))
 
-total = (bill*((tip/100)+1))/people
+total = (bill * ((tip / 100) + 1)) / people
 
-print(f"Each person should pay: ${total:.2f}")
+roundabout = round(total, 2)
+
+print(f"Each person should pay: ${roundabout}")
