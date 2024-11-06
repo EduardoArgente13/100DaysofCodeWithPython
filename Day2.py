5175113c6ee45c04cc0591e69761bc6354e0f218
Eduardo Argente <eduardoargente.dev@gmail.com>
2024-11-06 11:59:48 -0600

    Second Project

---
+++ Day2.py
@@ -0,0 +1,9 @@
+# Second project
+print("Hello! Welcome to the tip calculator!")
+bill = int(input("Whats the total bill?: "))
+tip = int(input("How much tip would you like to give?: "))
+people = int(input("How many people to split the bill?: "))
+
+total = (bill*((tip/100)+1))/people
+
+print(f"Each person should pay: ${total:.2f}")
